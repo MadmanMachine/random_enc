2dc050ea94cbf3048e77829ea40ed57cd2f7faba
Geoff_Schindler <geoffs.gears@gmail.com>
2011-04-12 20:59:01 -0500

    start game

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandomEnc::Application.config.secret_token = 'ae45a43b831516c1a52835a5417bff0701fc06eba5017eb1a4af51d3439fdc90379f9bff6dfc6d612a9274375bae029133267f8fe0867f68e4a39b07a1ce95c9'
---
+++ db/migrate/20110413014857_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.integer :strength
+      t.integer :dexterity
+      t.integer :endurance
+      t.integer :wins
+      t.integer :losses
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
